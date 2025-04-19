8768e8f8feac838dba21e63184978364c98a1b82
Zahraa <zahramss75@gmail.com>
2025-04-19 21:01:06 +0200

    added matches lines

--- sift.py
+++ sift.py
@@ -6,66 +6,68 @@ import matplotlib.pyplot as plt
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Feature Matching and Object Detection using SIFT")
 parser.add_argument('--query', required=True, help="Path to the query image (object)")
-parser.add_argument('--target', required=True, help="Path to the target image (scene)")
-parser.add_argument('--output', required=True, help="Path to save the output image")
 args = parser.parse_args()
 
 # Load images
-query_img = cv2.imread(args.query, cv2.IMREAD_GRAYSCALE)
-target_img = cv2.imread(args.target, cv2.IMREAD_GRAYSCALE)
-target_color = cv2.imread(args.target)
+query_image_gray = cv2.imread(f'queries/{args.query}', cv2.IMREAD_GRAYSCALE)
+query_image_color = cv2.imread(f'queries/{args.query}')
 
-if query_img is None or target_img is None:
+target_image_gray = cv2.imread('target.jpg', cv2.IMREAD_GRAYSCALE)
+target_image_color = cv2.imread('target.jpg')
+
+if query_image_gray is None or target_image_gray is None:
     print("Error loading images.")
     exit()
 
 # SIFT detector
 sift = cv2.SIFT_create()
 
-kp1, des1 = sift.detectAndCompute(query_img, None)
-kp2, des2 = sift.detectAndCompute(target_img, None)
+query_keypoints, query_descriptors = sift.detectAndCompute(query_image_gray, None)
+target_keypoints, target_descriptors = sift.detectAndCompute(target_image_gray, None)
 
-# FLANN matcher
+# FLANN matcher 
 FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
 search_params = dict(checks=50)
-
-flann = cv2.FlannBasedMatcher(index_params, search_params)
-matches = flann.knnMatch(des1, des2, k=2)
+flann_matcher = cv2.FlannBasedMatcher(index_params, search_params)
+matches = flann_matcher.knnMatch(query_descriptors, target_descriptors, k=2)
 
 # Lowe's ratio test
-good_matches = []
-for m, n in matches:
-    if m.distance < 0.75 * n.distance:
-        good_matches.append(m)
+good_matches = [m for m, n in matches if m.distance < 0.75 * n.distance]
 
-if len(good_matches) > 10:
-    src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+src_pts = np.float32([query_keypoints[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+dst_pts = np.float32([target_keypoints[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
-    # Homography
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+# Homography
+homography_matrix, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
-    h, w = query_img.shape
-    pts = np.float32([[0, 0], [0, h], [w, h], [w, 0]]).reshape(-1, 1, 2)
-    dst = cv2.perspectiveTransform(pts, M)
+# Rectangle around the detected object
+h, w = query_image_gray.shape
+object_points = np.float32([[0, 0], [0, h], [w, h], [w, 0]]).reshape(-1, 1, 2)
+transformed_points = cv2.perspectiveTransform(object_points, homography_matrix)
 
-    # Draw rectangle
-    detected = cv2.polylines(target_color, [np.int32(dst)], isClosed=True, color=(0, 255, 0), thickness=4)
+# Draw matches and detected rectangle
+matches_image = cv2.drawMatches(query_image_color, query_keypoints, target_image_color, target_keypoints, good_matches, None, flags=2)
+detected_image = cv2.polylines(target_image_color, [np.int32(transformed_points)], isClosed=True, color=(0, 255, 0), thickness=4)
 
-    # Resize for display
-    display_width = 1000
-    scale = display_width / detected.shape[1]
-    display_height = int(detected.shape[0] * scale)
-    resized = cv2.resize(detected, (display_width, display_height))
+# Convert to RGB for matplotlib    
+matches_rgb = cv2.cvtColor(matches_image, cv2.COLOR_BGR2RGB)
+detected_rgb = cv2.cvtColor(detected_image, cv2.COLOR_BGR2RGB)
 
-    # Save and plot
-    cv2.imwrite(args.output, detected)
-    rgb_image = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
-    plt.imshow(rgb_image)
-    plt.axis('off')
-    plt.title('Detected Object')
-    plt.show()
+# Save and display images
+cv2.imwrite(f'outputs/matched-{args.query}', matches_image)
+cv2.imwrite(f'outputs/detected-{args.query}', detected_image)
 
-else:
-    print("Not enough good matches were found.")
+plt.figure(figsize=(10, 5))
+
+plt.subplot(1, 2, 1)
+plt.imshow(matches_rgb)
+plt.title('Feature Matches')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(detected_rgb)
+plt.title('Detected Object')
+plt.axis('off')
+
+plt.show()
